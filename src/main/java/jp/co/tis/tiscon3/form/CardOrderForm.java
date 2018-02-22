86ccf1ca59a7b7ec4e507ab1971c4bdaa3c2e4e4
naoka001 <nao10.14.3124.a16@gmail.com>
2018-02-23 04:13:44 +0900

    エラーメッセージの表示形式の変更(お勤め先も)。TOP画像の横並び。

--- src/main/java/jp/co/tis/tiscon3/form/CardOrderForm.java
+++ src/main/java/jp/co/tis/tiscon3/form/CardOrderForm.java
@@ -13,19 +13,19 @@ public class CardOrderForm extends FormBase {
     @Size(max = 60)
     private String kanjiName;
 
-    @NotBlank(message="お名前(カナ)を必ず入力してください")
+    @NotBlank(message="お名前(カタカナ)を必ず入力してください")
     @Size(max = 90)
-    @Pattern(regexp = "^[ァ-ヶー 　]*$")
+    @Pattern(regexp = "^[ァ-ヶー 　]*$", message="お名前（カタカナ）はカタカナで入力してください")
     private String kanaName;
 
     @NotBlank(message="お名前(ローマ字)を必ず入力してください")
     @Size(max = 120)
-    @Pattern(regexp = "^[a-zA-Z 　]*$")
+    @Pattern(regexp = "^[a-zA-Z 　]*$", message="お名前（ローマ字）はローマ字で入力してください")
     private String alphabetName;
 
     @NotBlank(message="生年月日を必ず入力してください")
     @Size(max = 10)
-    @Pattern(regexp = "\\d{4}/\\d{1,2}/\\d{1,2}$")
+    @Pattern(regexp = "\\d{4}/\\d{1,2}/\\d{1,2}$", message="生年月日は半角数字で入力してください")
     private String dateOfBirth;
 
     @NotBlank(message="性別を必ず入力してください")
@@ -34,7 +34,7 @@ public class CardOrderForm extends FormBase {
 
     @NotBlank(message="郵便番号を必ず入力してください")
     @Size(max = 7)
-    @Pattern(regexp = "^[0-9]{3}[0-9]{4}$")
+    @Pattern(regexp = "^[0-9]{3}[0-9]{4}$", message="郵便番号は半角数字で入力してください")
     private String zipCode;
 
     @NotBlank(message="住所を必ず入力してください")
@@ -43,11 +43,11 @@ public class CardOrderForm extends FormBase {
 
     @NotBlank(message="自宅電話番号を必ず入力してください")
     @Size(max = 10)
-    @Pattern(regexp = "^0[0-9]{1,3}[0-9]{2,4}[0-9]{4}$")
+    @Pattern(regexp = "^0[0-9]{1,3}[0-9]{2,4}[0-9]{4}$", message="自宅電話番号は半角数字で入力してください")
     private String homePhoneNumber;
 
     @Size(max = 13)
-    @Pattern(regexp = "^((070|080|090)[0-9]{4}-[0-9]{4})?$")
+    @Pattern(regexp = "^((070|080|090)[0-9]{4}-[0-9]{4})?$", message="携帯電話番号は半角数字で入力してください")
     private String mobilePhoneNumber;
 
     @NotBlank(message="メールアドレスを必ず入力してください")
@@ -55,7 +55,7 @@ public class CardOrderForm extends FormBase {
     @Email
     private String emailAddress;
 
-    @NotBlank(message="自宅電話番号を必ず入力してください")
+    @NotBlank(message="配偶者を必ず入力してください")
     @Size(max = 20)
     private String spouse;
 
@@ -65,7 +65,7 @@ public class CardOrderForm extends FormBase {
 
     @NotBlank(message="ローン等のお借り入れ金額を必ず入力してください")
     @Size(max = 6)
-    @Pattern(regexp = "[0-9]*")
+    @Pattern(regexp = "[0-9]*", message="お借り入れ金額は半角数字で入力してください")
     private String debt;
 
     @NotBlank(message="ご職業を必ず入力してください")
@@ -74,21 +74,21 @@ public class CardOrderForm extends FormBase {
 
     @NotBlank(message="昨年の所得を必ず入力してください")
     @Size(max = 6)
-    @Pattern(regexp = "[0-9]*")
+    @Pattern(regexp = "[0-9]*", message="昨年の所得は半角数字で入力してください")
     private String income;
 
     @Size(max = 255)
     private String employerName;
 
     @Size(max = 8)
-    @Pattern(regexp = "^([0-9]{3}-[0-9]{4})?$")
+    @Pattern(regexp = "^([0-9]{3}[0-9]{4})?$", message="郵便番号は半角数字で入力してください")
     private String employerZipCode;
 
     @Size(max = 255)
     private String employerAddress;
 
     @Size(max = 13)
-    @Pattern(regexp = "^(0[0-9]{1,3}-[0-9]{2,4}-[0-9]{4})?$")
+    @Pattern(regexp = "^(0[0-9]{1,3}[0-9]{2,4}[0-9]{4})?$", message="自宅電話番号は半角数字で入力してください")
     private String employerPhoneNumber;
 
     @Size(max = 255)
@@ -109,7 +109,7 @@ public class CardOrderForm extends FormBase {
     private String department;
 
     @Size(max = 6)
-    @Pattern(regexp = "[0-9]*")
+    @Pattern(regexp = "[0-9]*", message="勤続年数は半角数字で入力してください")
     private String employeeLength;
 
 }
